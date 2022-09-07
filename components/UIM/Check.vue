75a5df61a938fab5a1bd833436bb6f4245b1213e
Abel Suárez <lebazeraus@gmail.com>
2022-09-07 10:35:45 -0500

    202209071035

---
+++ components/UIM/Check.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { CSSAlignItems, CSSArtifactMiselanea, CSSGap, CSSGridTemplateColumns } from "~/composables/useCSS"
+
+const props = defineProps({
+  active: { type: Boolean },
+  bgHover: { type: String, default: 'graylight' },
+  color: { type: String, default: 'gray' },
+  colorActive: { type: String, default: 'black' },
+  colorHover: { type: String, default: 'black' },
+  isRadio: { type: Boolean }
+})
+</script>
+
+<template>
+<div :class="[CSSAlignItems.center, CSSArtifactMiselanea.grid, CSSGap._8, CSSGridTemplateColumns.max_1fr]">
+  <UIAIcon
+    @click="$emit('click', !props.active)"
+    :bg-hover="props.bgHover"
+    :br-radius="props.isRadio ? 50 : 4"
+    :class="[CSSArtifactMiselanea.cursor_pointer]"
+    :color="props.active ? props.colorActive : props.color"
+    :color-hover="props.colorHover"
+    :name="props.isRadio ? props.active ? 'check-circle' : 'circle' : props.active ? 'check-square' : 'square-full'" />
+  <UIALabel line-clamp="1"><slot/></UIALabel>
+</div>
+</template>
