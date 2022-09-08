bef80e7e5f4e9362c08ae52bbbcb66f164214c0c
Abel Suárez <lebazeraus@gmail.com>
2022-09-08 11:33:22 -0500

    202209081133

--- components/UIA/Label.vue
+++ components/UIA/Label.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { computed } from "@nuxtjs/composition-api"
-import { CSSArtifactMiselanea, CSSColor, CSSTextAlign, CSSFontWeight, CSSMargin, CSSTextSize } from "~/composables/useCSS"
+import { CSSArtifactMiselanea, CSSBorderRadius, CSSColor, CSSTextAlign, CSSFontWeight, CSSMargin, CSSPadding, CSSTextSize } from "~/composables/useCSS"
 
 const props = defineProps({
   align: { type: String },
   bg: { type: String },
+  brRadius: { type: String },
   color: { type: String, default: 'black' },
   colorHover: { type: String },
   isBold: { type: Boolean },
+  isCapital: { type: Boolean },
   isCapital: { type: Boolean },
   isUpper: { type: Boolean },
   lineClamp: { type: [String, Number] },
@@ -16,6 +18,11 @@ const props = defineProps({
   marginLeft: { type: [String, Number] },
   marginRight: { type: [String, Number] },
   marginTop: { type: [String, Number] },
+  padding: { type: [String, Number] },
+  paddingBottom: { type: [String, Number] },
+  paddingLeft: { type: [String, Number] },
+  paddingRight: { type: [String, Number] },
+  paddingTop: { type: [String, Number] },
   size: { type: [String, Number], default: 14 },
   text: { type: [String, Number] }
 })
@@ -26,15 +33,19 @@ const getCSSALabel = computed(() => {
     CSSArtifactMiselanea[`line_clamp_${props.lineClamp}`],
     props.isCapital ? CSSArtifactMiselanea.text_capitalize : null,
     props.isUpper ? CSSArtifactMiselanea.text_uppercase : null,
+    CSSBorderRadius[`_${props.brRadius}`],
     CSSColor[`bg_${props.bg}`],
     CSSColor[`text_${props.color}`],
     CSSColor[`text_hover_${props.colorHover}`],
     props.isBold ? CSSFontWeight._600 : null,
-    CSSMargin[`_${props.margin}`],
-    CSSMargin[`bottom_${props.marginBottom}`],
-    CSSMargin[`left_${props.marginLeft}`],
-    CSSMargin[`right_${props.marginRight}`],
-    CSSMargin[`top_${props.marginTop}`],
+    CSSMargin[`bottom_${props.marginBottom || props.margin}`],
+    CSSMargin[`left_${props.marginLeft || props.margin}`],
+    CSSMargin[`right_${props.marginRight || props.margin}`],
+    CSSMargin[`top_${props.marginTop || props.margin}`],
+    CSSPadding[`bottom_${props.paddingBottom || props.padding}`],
+    CSSPadding[`left_${props.paddingLeft || props.padding}`],
+    CSSPadding[`right_${props.paddingRight || props.padding}`],
+    CSSPadding[`top_${props.paddingTop || props.padding}`],
     CSSTextAlign[props.align],
     CSSTextSize[`_${props.size}`]
   ].filter($ => $)
--- components/UIM/Check.vue
+++ components/UIM/Check.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { CSSAlignItems, CSSArtifactMiselanea, CSSGap, CSSGridTemplateColumns } from "~/composables/useCSS"
+import { computed } from '@nuxtjs/composition-api'
+import { CSSAlignItems, CSSArtifactDisabled, CSSArtifactMiselanea, CSSGap, CSSGridTemplateColumns } from "~/composables/useCSS"
 
 const props = defineProps({
   active: { type: Boolean },
@@ -7,12 +8,22 @@ const props = defineProps({
   color: { type: String, default: 'gray' },
   colorActive: { type: String, default: 'black' },
   colorHover: { type: String, default: 'black' },
+  disabled: { type: Boolean },
   isRadio: { type: Boolean }
+})
+
+const getCSSACheck = computed(() => {
+  return [
+    CSSAlignItems.center,
+    CSSArtifactMiselanea.grid,
+    CSSGap._8,
+    CSSGridTemplateColumns.max_1fr
+  ].filter($ => $)
 })
 </script>
 
 <template>
-<div :class="[CSSAlignItems.center, CSSArtifactMiselanea.grid, CSSGap._8, CSSGridTemplateColumns.max_1fr]">
+<div :class="[getCSSACheck]">
   <UIAIcon
     @click="$emit('click', !props.active)"
     :bg-hover="props.bgHover"
@@ -20,7 +31,10 @@ const props = defineProps({
     :class="[CSSArtifactMiselanea.cursor_pointer]"
     :color="props.active ? props.colorActive : props.color"
     :color-hover="props.colorHover"
+    :disabled="props.disabled"
     :name="props.isRadio ? props.active ? 'check-circle' : 'circle' : props.active ? 'check-square' : 'square-full'" />
-  <UIALabel line-clamp="1"><slot/></UIALabel>
+  <div :class="CSSArtifactMiselanea.flex">
+    <UIALabel :class="props.disabled ? CSSArtifactDisabled.check : null" is-inline-block line-clamp="1"><slot/></UIALabel>
+  </div>
 </div>
 </template>
